a54ad245471d6f55ee3d2eefed48731de7005481
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-10-03 19:58:13 +0530

    did some testings and registration basics are done

---
+++ src/main/java/com/the_polar_lights/spring_boot_crud_app/service/userService.java
@@ -0,0 +1,2 @@
+package com.the_polar_lights.spring_boot_crud_app.service;public class userService {
+}
---
+++ src/test/java/com/the_polar_lights/spring_boot_crud_app/test/userRepoTest.java
@@ -0,0 +1,5 @@
+package com.the_polar_lights.spring_boot_crud_app.test;
+
+@DataJpaTest
+public class userRepoTest {
+}
