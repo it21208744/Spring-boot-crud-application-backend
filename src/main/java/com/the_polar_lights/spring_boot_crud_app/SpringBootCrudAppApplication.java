c0c16da207f13e04e042123f427dac21c7831772
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-10-03 12:12:10 +0530

    Initial commit

---
+++ src/main/java/com/the_polar_lights/spring_boot_crud_app/SpringBootCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.the_polar_lights.spring_boot_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/the_polar_lights/spring_boot_crud_app/SpringBootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_polar_lights.spring_boot_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
