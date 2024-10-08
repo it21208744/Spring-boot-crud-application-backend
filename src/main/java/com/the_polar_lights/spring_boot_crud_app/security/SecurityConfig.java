427e47cfd38d07d0d93268e1dce7e6ce485883cc
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-10-08 17:27:53 +0530

    password encryption and safe routes defined

--- src/main/java/com/the_polar_lights/spring_boot_crud_app/security/SecurityConfig.java
+++ src/main/java/com/the_polar_lights/spring_boot_crud_app/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.the_polar_lights.spring_boot_crud_app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,5 +28,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
