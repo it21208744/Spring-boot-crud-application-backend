c41142db4bfc8a2190d2e83c4d84cf4928de5bc6
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-10-08 15:17:30 +0530

    updated spring security of login and register to be accessible without authentication

--- src/main/java/com/the_polar_lights/spring_boot_crud_app/security/SecurityConfig.java
+++ src/main/java/com/the_polar_lights/spring_boot_crud_app/security/SecurityConfig.java
@@ -1,46 +1,22 @@
-//package com.the_polar_lights.spring_boot_crud_app.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfiguration {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/api/auth/**").permitAll()
-//                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-//                        .anyRequest().authenticated()
-//                )
-//                .httpBasic(withDefaults());
-//        return http.build();
-//
-//    }
-//
-//
-//    @Bean
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
-//                .and()
-//                .withUser("user").password(passwordEncoder().encode("user123")).roles("USER");
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//
-//}
+package com.the_polar_lights.spring_boot_crud_app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/users/register", "/users/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(customizer -> customizer.disable());
+
+        return http.build();
+    }
+}
