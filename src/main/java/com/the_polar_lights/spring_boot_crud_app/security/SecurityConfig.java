f8a65cbb449e34695190bf96e0f4014ddc5ba65f
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-10-08 15:32:14 +0530

    user registration password encryption

--- src/main/java/com/the_polar_lights/spring_boot_crud_app/security/SecurityConfig.java
+++ src/main/java/com/the_polar_lights/spring_boot_crud_app/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.the_polar_lights.spring_boot_crud_app.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,11 @@ public class SecurityConfig {
                 .csrf(customizer -> customizer.disable());
 
         return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/the_polar_lights/spring_boot_crud_app/service/userService.java
+++ src/main/java/com/the_polar_lights/spring_boot_crud_app/service/userService.java
@@ -4,6 +4,7 @@ import com.the_polar_lights.spring_boot_crud_app.Entity.userEntity;
 import com.the_polar_lights.spring_boot_crud_app.repository.userRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,15 +12,27 @@ import java.util.Optional;
 
 @Service
 public class userService {
-    @Autowired
-    private userRepository userRepo;
+
+    private final userRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Autowired userService(userRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public userEntity createUser(userEntity user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
+
 
     //get all users
     //get user by id
     //create user
-    public userEntity createUser(userEntity user){
-        return userRepo.save(user);
-    }
+//    public userEntity createUser(userEntity user){
+//        return userRepo.save(user);
+//    }
     //update user
     //delete user
 
